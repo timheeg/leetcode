bd6c521a4d73a6ef5cc733443612a091ab37bbd6
Timothy Heeg <timheeg@users.noreply.github.com>
2024-03-17 19:26:10 -0400

    ✅ Add leetcode defined test cases for 80

---
+++ tests/80/leetcode.test.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <solution.h>
+
+#include <cassert>
+
+TEST(LeetCodeTest, Case1) {
+  std::vector<int> nums = {1, 1, 1, 2, 2, 3};
+
+  Solution s;
+  auto k = s.removeDuplicates(nums);
+
+  std::vector<int> expected = {1, 1, 2, 2, 3};
+  EXPECT_EQ(k, expected.size());
+  for (int i = 0; i < k; i++) {
+    assert(nums[i] == expected[i]);
+  }
+}
+
+TEST(LeetCodeTest, Case2) {
+  std::vector<int> nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+
+  Solution s;
+  auto k = s.removeDuplicates(nums);
+
+  std::vector<int> expected = {0, 0, 1, 1, 2, 3, 3};
+  EXPECT_EQ(k, expected.size());
+  for (int i = 0; i < k; i++) {
+    assert(nums[i] == expected[i]);
+  }
+}
