eb6811fb273b35b3914ef0aed1a942db978d8e37
Timothy Heeg <timheeg@users.noreply.github.com>
2024-03-18 17:40:02 -0400

    ✅ Add leetcode test cases for 121

---
+++ tests/121/leetcode.test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <solution.h>
+
+TEST(LeetCodeTest, Case1) {
+  std::vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+  Solution s;
+  auto profit = s.maxProfit(prices);
+
+  int expected{5};
+  EXPECT_EQ(profit, expected);
+}
+
+TEST(LeetCodeTest, Case2) {
+  std::vector<int> prices = {7, 6, 4, 3, 1};
+
+  Solution s;
+  auto profit = s.maxProfit(prices);
+
+  int expected{0};
+  EXPECT_EQ(profit, expected);
+}
