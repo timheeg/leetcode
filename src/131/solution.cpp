d86a297b8bb3f9bd0cddd233d093087364b9ec25
Timothy Heeg <timheeg@users.noreply.github.com>
2024-04-26 18:22:35 -0400

    🐌 Add brute force solution for 131

---
+++ src/131/solution.cpp
@@ -0,0 +1,45 @@
+#include <131/solution.h>
+
+#include <131/brute-force.cpp>
+
+namespace brute_force {
+int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost) {
+  // Number of stops along the circuit
+  const auto n = gas.size();
+
+  // Loop through each possible starting location
+  for (auto i = 0u; i < gas.size(); ++i) {
+    if (gas[i] < cost[i]) {
+      std::cout << "Location " << i << " doesn't have enough gas to start."
+                << std::endl;
+      continue;
+    }
+
+    // Fill gas tank and attempt this circuit
+    auto tank{0};
+    for (auto j = 0u; j < gas.size(); ++j) {
+      tank += gas[(i + j) % n];
+      tank -= cost[(i + j) % n];
+      if (tank < 0) {
+        std::cout << "Ran out of gas. ⛽ Cannot make it to next stop "
+                  << ((i + j) % n) + 1 << std::endl;
+        break;
+      }
+    }
+    if (tank >= 0) {
+      std::cout << "Made it!! Starting from location " << i << " with " << tank
+                << " gallons of gas left!" << std::endl;
+      return i;
+    }
+  }
+  std::cout << "No possible circuits found. 😢" << std::endl;
+  return -1;
+}
+}  // namespace brute_force
+
+int Solution::canCompleteCircuit(std::vector<int>& gas,
+                                 std::vector<int>& cost) {
+#if 1
+  return brute_force::canCompleteCircuit(gas, cost);
+#endif
+}
---
+++ src/131/solution.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+ public:
+  int canCompleteCircuit(std::vector<int>&, std::vector<int>&);
+};
