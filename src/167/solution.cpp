4b759efab6e0fd80fcdcda5c0f30c59ce3d599dd
Timothy Heeg <timheeg@users.noreply.github.com>
2024-05-23 08:39:33 -0400

    💚 Solution for 167

---
+++ src/167/solution.cpp
@@ -0,0 +1,5 @@
+#include <167/solution.h>
+
+std::vector<int> Solution::twoSum(std::vector<int>& numbers, int target) {
+  return {};
+}
---
+++ src/167/solution.h
@@ -0,0 +1,6 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> twoSum(std::vector<int>& numbers, int target);
+};
